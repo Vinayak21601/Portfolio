14246742b8ee185feececd9245dc5fcfe7ee72e8
Vinayakuponly <vinayak.devare@uponly.in>
2023-12-24 22:43:47 +0530

    first push

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+// import HeroBgAnimation from "../HeroBgAnimation";
+import {
+  HeroContainer,
+  HeroBg,
+  HeroLeftContainer,
+  Img,
+  HeroRightContainer,
+  HeroInnerContainer,
+  TextLoop,
+  Title,
+  Span,
+  SubTitle,
+  // SocialMediaIcons,
+  // SocialMediaIcon,
+  ResumeButton,
+} from "../Home/HomeStyle";
+import Profilepic from "../../assets/images/profilepic.jpeg";
+import Typewriter from "typewriter-effect";
+
+import { Bio } from "../../Data/constants";
+import HeroBgAnimation from "../HeroBgAnimation/BgAnimation";
+
+const Home = () => {
+  return (
+    <div id="about">
+      <HeroContainer>
+        <HeroBg>
+          <HeroBgAnimation />
+        </HeroBg>
+        <HeroInnerContainer>
+          <HeroLeftContainer id="Left">
+            <Title>
+              Hi, I am <br /> {Bio.name}
+            </Title>
+            <TextLoop>
+              I am a
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            <SubTitle>{Bio.description}</SubTitle>
+            <ResumeButton href={Bio.resume} target="display">
+              Check Resume
+            </ResumeButton>
+          </HeroLeftContainer>
+
+          <HeroRightContainer id="Right">
+            <Img src={Profilepic} alt="Profilepic-image" />
+          </HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
+};
+
+export default Home;
